b418dc2ef2d9bc191b798f315ecd957c247c917b
Doug Ryan <ryan2135@pacificu.edu>
2022-02-04 10:11:01 -0800

    Add project files.

---
+++ DNAStrand/main.cpp
@@ -0,0 +1,31 @@
+//********************************************************************* 
+// File name:	 
+// Author:		
+// Date:			 
+// Class:			 
+// Assignment: 
+// Purpose:		 
+//*********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+/**********************************************************************
+Function:		  main
+
+Description:	main driver used to test a DNAStrand object. This driver
+							will accept a DNAStrand in FASTA format from the 
+							keyboard and then read in two strings from a file. The
+							reverse complement, GC content, and hamming distance of
+							two strands will be displayed.
+
+Parameters:		none
+
+Returned:			exit status
+**********************************************************************/
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
